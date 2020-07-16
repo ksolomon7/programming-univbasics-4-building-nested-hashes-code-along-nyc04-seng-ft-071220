abfb12c20e6d219db69c744d8561a786459df2c1
Kalkidan Taye <kalkidan.solomon2@gmail.com>
2020-07-16 18:44:10 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -8,9 +8,9 @@ def second_challenge
       #your key/value pairs here!
       },
     :capulet => {
-      :patriarch =>{},
-      :matriarch =>{},
-      :heroine =>{},
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
       :heroine_friends =>[]
       #your key/value pairs here!
       }}
